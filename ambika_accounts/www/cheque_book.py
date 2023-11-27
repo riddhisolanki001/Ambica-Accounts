d568b86d9a36c7a2bae901e218e93fc8e8c7a92a
Aditiya <aditiya@sanskartechnolab.com>
2023-11-27 18:09:01 +0530

    Doctype

--- ambika_accounts/www/cheque_book.py
+++ ambika_accounts/www/cheque_book.py
@@ -1,295 +1,114 @@
 import frappe
 
-
 # @frappe.whitelist()
 # def cheque_number():
-#     query = "SELECT from_no, last_no FROM `tabCheque Book` WHERE last_no >= from_no ORDER BY `creation` DESC LIMIT 1;"
+#     # Fetch the last 'tabCheque Book' record
+#     last_cheque_record = frappe.get_last_doc('Cheque Book')
+#     last_payment_record = frappe.get_last_doc('Payment Entry')
+#     print("/n/n/n/n/n/n/n")
+#     print(last_cheque_record.last_no)
+#     print(int(last_payment_record.reference_no)+1)
 
-#     # Fetch the record using the query
-#     result = frappe.db.sql(query, as_dict=True)
-#     x="SELECT name, reference_no FROM `tabPayment Entry` WHERE status = 0 AND docstatus = 1 ORDER BY creation DESC LIMIT 1;"
-    
-#     if result:
-#         last_record = result[0]
-#         frappe.errprint(last_record)
-#         return frappe._dict(last_record) 
+#     if last_cheque_record.last_no == str(int(last_payment_record.reference_no)+1):
+#         frappe.msgprint(f"Last Payment Record hiiiii: {str(last_cheque_record.reference_no)}")
+         
 #     else:
-#         frappe.errprint("No records found.")
-#         return None
-# @frappe.whitelist()
-# def cheque_number():
-#     # Fetch the last record from 'tabCheque Book'
-#     cheque_book_query = """
-#         SELECT name, from_no, last_no
-#         FROM `tabCheque Book`
-#         WHERE last_no >= from_no
-#         ORDER BY `creation` DESC
-#         LIMIT 1;
-#     """
-#     cheque_book_result = frappe.db.sql(cheque_book_query, as_dict=True)
+#         frappe.msgprint(f"Last Payment Record: {str(last_payment_record)}")
+#         new_last_no = int(last_payment_record.reference_no) + 1
+#         return new_last_no
 
-#     if cheque_book_result:
-#         last_cheque_record = cheque_book_result[0]
-
-#         # Fetch the last record from 'tabPayment Entry'
-#         payment_entry_query = """
-#             SELECT name, reference_no
-#             FROM `tabPayment Entry`
-#             WHERE status = 0 AND docstatus = 1
-#             ORDER BY creation DESC
-#             LIMIT 1;
-#         """
-#         payment_entry_result = frappe.db.sql(payment_entry_query, as_dict=True)
-
-#         if payment_entry_result and last_cheque_record:
-#             last_payment_record = payment_entry_result[0]
-#             frappe.msgprint(str(last_payment_record))
-#             new_last_no = int(last_payment_record['reference_no']) + 1
-
-#             # Update 'tabCheque Book' with the new_last_no value
-#             # frappe.db.set_value('Cheque Book', last_cheque_record['name'], 'last_no', new_last_no)
-
-#             # Return the last record from 'tabCheque Book'
-#             return new_last_no
-#         else:
-#             frappe.errprint("No records found in 'tabPayment Entry'.")
-#             return None
-#     else:
-#         frappe.errprint("No records found in 'tabCheque Book'.")
-#         return None
 @frappe.whitelist()
+# def cheque_number():
+#     # Fetch the last 'tabCheque Book' record
+#     last_cheque_record = frappe.get_last_doc('Cheque Book')
+#     last_payment_record = frappe.get_last_doc('Payment Entry')
+
+#     print("\n\n\n\n\n\n\n")
+#     print(last_cheque_record.last_no)
+#     x=int(last_payment_record.reference_no) + 1
+#     print(x)
+
+#     if last_cheque_record.last_no == str(x):
+#         frappe.msgprint(f"Last Payment Record: {str(last_cheque_record.reference_no)}")
+        
+#     else:
+#         frappe.msgprint(f"Last Payment Record: {str(last_payment_record)}")
+#         new_last_no = int(last_payment_record.reference_no) + 1
+#         frappe.msgprint(f"New Last Number: {new_last_no}")
+
+  
+#     return new_last_no
 def cheque_number():
-    # Fetch the last record from 'tabCheque Book'
-    cheque_book_query = """
-        SELECT name, from_no, last_no
-        FROM `tabCheque Book`
-        WHERE last_no >= from_no
-        ORDER BY `creation` DESC
-        LIMIT 1;
-    """
-    cheque_book_result = frappe.db.sql(cheque_book_query, as_dict=True)
+    # Fetch the last 'tabCheque Book' record
+    last_cheque_record = frappe.get_last_doc('Cheque Book')
+    last_payment_record = frappe.get_last_doc('Payment Entry')
 
-    if cheque_book_result:
-        last_cheque_record = cheque_book_result[0]
+    print("\n\n\n\n\n\n\n")
+    print(last_cheque_record.last_no)
+    x = last_payment_record.reference_no
+    print(x)
 
-        # Fetch the last record from 'tabPayment Entry'
-        payment_entry_query = """
-            SELECT name, reference_no
-            FROM `tabPayment Entry`
-            WHERE status = 0 AND docstatus = 1
-            ORDER BY creation DESC
-            LIMIT 1;
-        """
-        payment_entry_result = frappe.db.sql(payment_entry_query, as_dict=True)
+    new_last_no = None  # Define new_last_no outside the if-else block
 
-        if payment_entry_result and last_cheque_record:
-            last_payment_record = payment_entry_result[0]
-
-            # Check if the value of 'reference_no' is equal to 'last_no'
-            if last_payment_record['reference_no'] == last_cheque_record['last_no']:
-                frappe.msgprint("Value of 'reference_no' is equal to 'last_no'. Breaking the condition.")
-                return None
-
-            frappe.msgprint(str(last_payment_record))
-            new_last_no = int(last_payment_record['reference_no']) + 1
-
-            # Update 'tabCheque Book' with the new_last_no value
-            # frappe.db.set_value('Cheque Book', last_cheque_record['name'], 'last_no', new_last_no)
-
-            # Return the last record from 'tabCheque Book'
-            return new_last_no
-        else:
-            frappe.errprint("No records found in 'tabPayment Entry'.")
-            return None
+    if last_cheque_record.last_no == str(x):
+        frappe.msgprint(f"Last Payment Record: {str(last_cheque_record.reference_no)}")
+        
     else:
-        frappe.errprint("No records found in 'tabCheque Book'.")
-        return None
+        frappe.msgprint(f"Last Payment Record: {str(last_payment_record)}")
+        # new_last_no = int(last_payment_record.reference_no) + 1
+        frappe.msgprint(f"New Last Number: {new_last_no}")
+
+    return new_last_no
 
 # @frappe.whitelist()
 # def cheque_number():
-#     # Fetch the last record from 'tabCheque Book'
-#     cheque_book_query = """
-#         SELECT name, from_no, last_no
-#         FROM `tabCheque Book`
-#         WHERE last_no <= from_no
-#         ;
-#     """
-#     cheque_book_result = frappe.db.sql(cheque_book_query, as_dict=True)
+#     # Fetch the last 'tabCheque Book' record
+#     last_cheque_record = frappe.get_last_doc('Cheque Book')
+#     last_payment_record = frappe.get_last_doc('Payment Entry')
 
-#     print("Cheque Book Result:", cheque_book_result)
+#     print("\n\n\n\n\n\n\n")
+#     print(last_cheque_record.last_no)
+#     x = int(last_payment_record.reference_no) + 1
 
-#     if cheque_book_result:
-#         last_cheque_record = cheque_book_result[0]
+#     # Initialize new_last_no to x
+#     new_last_no = x
 
-#         # Fetch the last record from 'tabPayment Entry'
-#         payment_entry_query = """
-#             SELECT name, reference_no
-#             FROM `tabPayment Entry`
-#             WHERE status = 0 AND docstatus = 1
-#             ORDER BY creation DESC
-#             LIMIT 1;
-#         """
-#         payment_entry_result = frappe.db.sql(payment_entry_query, as_dict=True)
-
-#         print("Payment Entry Result:", payment_entry_result)
-
-#         if payment_entry_result and last_cheque_record:
-#             last_payment_record = payment_entry_result[0]
-#             frappe.msgprint(str(last_payment_record))
-#             new_last_no = int(last_payment_record['reference_no']) + 1
-
-#             # Update 'tabCheque Book' with the new_last_no value
-#             # frappe.db.set_value('Cheque Book', last_cheque_record['name'], 'last_no', new_last_no)
-
-#             # Return the last record from 'tabCheque Book'
-#             return new_last_no
-#         else:
-#             frappe.errprint("No records found in 'tabPayment Entry'.")
-#             return None
+#     if last_cheque_record.last_no == str(x):
+#         frappe.msgprint(f"Last Payment Record: {str(last_cheque_record.reference_no)}")
 #     else:
-#         frappe.errprint("No records found in 'tabCheque Book'.")
-#         return None
+#         frappe.msgprint(f"Last Payment Record: {str(last_payment_record)}")
+#         # Modify new_last_no in the else block if needed
+#         new_last_no = int(last_payment_record.reference_no) + 1
+#         frappe.msgprint(f"New Last Number: {new_last_no}")
 
-# @frappe.whitelist()
-# def cheque_number():
-#     # Fetch the last record from 'tabCheque Book'
-#     cheque_book_query = """
-#         SELECT name, from_no, last_no
-#         FROM `tabCheque Book`
-#         WHERE last_no >= from_no;
-#     """
-#     cheque_book_result = frappe.db.sql(cheque_book_query, as_dict=True)
-
-#     print("Cheque Book Result:", cheque_book_result)
-
-#     if cheque_book_result:
-#         last_cheque_record = cheque_book_result[0]
-
-#         # Fetch the last record from 'tabPayment Entry'
-#         payment_entry_query = """
-#             SELECT name, reference_no
-#             FROM `tabPayment Entry`
-#             WHERE status = 0 AND docstatus = 1
-#             ORDER BY creation DESC
-#             LIMIT 1;
-#         """
-#         payment_entry_result = frappe.db.sql(payment_entry_query, as_dict=True)
-
-#         print("Payment Entry Result:", payment_entry_result)
-
-#         if payment_entry_result and last_cheque_record:
-#             last_payment_record = payment_entry_result[0]
-#             frappe.msgprint(str(last_payment_record))
-#             new_last_no = int(last_payment_record['reference_no']) + 1
-
-#             # Update 'tabCheque Book' with the new_last_no value
-#             # frappe.db.set_value('Cheque Book', last_cheque_record['name'], 'last_no', new_last_no)
-
-#             # Return the last record from 'tabCheque Book'
-#             return new_last_no
-#         else:
-#             frappe.errprint("No records found in 'tabPayment Entry'.")
-#             frappe.msgprint("No records found in 'tabPayment Entry'. This is the else block message.")
-#             return None
-#     else:
-#         frappe.errprint("No records found in 'tabCheque Book'.")
-#         frappe.msgprint("No records found in 'tabCheque Book'. This is the else block message.")
-#         return None
+#     # Move the return statement outside the if-else block
+#     return new_last_no
 
 # import frappe
+# from frappe import _
+# from frappe.model.document import Document
+
 
 # @frappe.whitelist()
-# def cheque_number(self):
-#     # Fetch the last record from tabCheque Book
-#     cheque_book_query = "SELECT from_no, last_no FROM `tabCheque Book` WHERE last_no >= from_no ORDER BY `creation` DESC LIMIT 1;"
-#     cheque_book_result = frappe.db.sql(cheque_book_query, as_dict=True)
-
-#     if cheque_book_result:
-#         cheque_book_record = cheque_book_result[0]
-#         frappe.errprint(cheque_book_record)
-#         last_no = cheque_book_record.get('last_no')
-
-#         # Increment the last_no value by 1
-#         next_cheque_number = last_no + 1
-
-#         # Update the last_no value in the tabCheque Book
-#         frappe.db.set_value('Cheque Book', cheque_book_record['name'], 'last_no', next_cheque_number)
-
-#         # Fetch the last reference number from tabPayment Entry
-#         payment_entry_query = "SELECT reference_no FROM `tabPayment Entry` WHERE status = 0 AND docstatus = 1 ORDER BY creation DESC LIMIT 1;"
-#         payment_entry_result = frappe.db.sql(payment_entry_query, as_dict=True)
-
-#         if payment_entry_result:
-#             last_reference_no = payment_entry_result[0].get('reference_no')
-
-#             # Increment the reference number
-#             next_reference_no = int(last_reference_no) + 1
-
-#             # Create a new payment entry with the incremented reference number
-#             new_payment_entry = frappe.get_doc({
-#                 'doctype': 'Payment Entry',
-#                 'reference_no': next_reference_no,
-#                 # Add other fields as needed
-#             })
 # def cheque_number():
-#     # Specify the upper and lower bounds for the range
-#     from_no = 100
-#     to_no = 200
+#     # Fetch the last 'Cheque Book' record
+#     last_cheque_record = frappe.get_last_doc('Cheque Book')
+#     print(last_cheque_record.last_no)
+    
+#     # print(last_number)
+#     last_payment_record = frappe.get_last_doc('Payment Entry')
+#     # Fetch the last 'Payment Entry' record
+#     # last_reference = frappe.db.get_value('Payment Entry', 'ACC-PAY-2023-00023','reference_no')
+#     # print(last_reference)
 
-#     # Fetch the records from 'tabCheque Book' where last_no is between from_no and to_no
-#     cheque_book_query = """
-#         SELECT name, from_no, last_no
-#         FROM `tabCheque Book`
-#         WHERE last_no BETWEEN %s AND %s
-#         ORDER BY `creation` DESC
-#         LIMIT 1;
-#     """
-#     cheque_book_result = frappe.db.sql(cheque_book_query, (from_no, to_no), as_dict=True)
+#     if last_cheque_record.last_no == last_payment_record.reference_no:
+#         frappe.msgprint(f"Last Payment Record: ")
 
-#     print("Cheque Book Result:", cheque_book_result)
+#     #     frappe.msgprint(f"Last Payment Record: ")
+#     #     return  # This will exit the function if the condition is true
+#     # elif last_number != last_reference:
+#     #     # frappe.msgprint(f"Last Payment Record: {str(last_payment_record.reference_no)}")
+#     #     new_last_no = int(last_payment_record.reference_no) + 1
+#     #     frappe.msgprint(f"New Last Number: {new_last_no}")
+#     # return new_last_no
 
-#     if cheque_book_result:
-#         last_cheque_record = cheque_book_result[0]
-
-#         # Fetch the last record from 'tabPayment Entry'
-#         payment_entry_query = """
-#             SELECT name, reference_no
-#             FROM `tabPayment Entry`
-#             WHERE status = 0 AND docstatus = 1
-#             ORDER BY creation DESC
-#             LIMIT 1;
-#         """
-#         payment_entry_result = frappe.db.sql(payment_entry_query, as_dict=True)
-
-#         print("Payment Entry Result:", payment_entry_result)
-
-#         if payment_entry_result :
-#             last_payment_record = payment_entry_result[0]
-#             frappe.msgprint(str(last_payment_record))
-#             new_last_no = int(last_payment_record['reference_no']) + 1
-
-#             # Update 'tabCheque Book' with the new_last_no value
-#             # frappe.db.set_value('Cheque Book', last_cheque_record['name'], 'last_no', new_last_no)
-
-#             # Return the last record from 'tabCheque Book'
-#             return new_last_no
-#         else:
-#             frappe.errprint("No records found in 'tabPayment Entry'.")
-#             frappe.msgprint("No records found in 'tabPayment Entry'. This is the else block message.")
-#             return None
-#     else:
-#         frappe.errprint("No records found in 'tabCheque Book'.")
-#         frappe.msgprint("No records found in 'tabCheque Book'. This is the else block message.")
-#         return None
-#             new_payment_entry.insert()
-
-#             return {
-#                 'next_cheque_number': next_cheque_number,
-#                 'next_reference_no': next_reference_no,
-#             }
-#         else:
-#             frappe.errprint("No records found in tabPayment Entry.")
-#             return None
-#     else:
-#         frappe.errprint("No records found in tabCheque Book.")
-#         return None
