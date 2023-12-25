b4224600f9973f79819a7a4e837d28fcae47d827
riddhi_solanki <riddhssolanki048@gmail.com>
2023-12-25 14:39:37 +0530

    client script

--- ambika_accounts/ambika_accounts/doctype/auto_outstanding/auto_outstanding.js
+++ ambika_accounts/ambika_accounts/doctype/auto_outstanding/auto_outstanding.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Riddhi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Auto Outsatnding", {
+// frappe.ui.form.on("Auto Outstanding", {
 // 	refresh(frm) {
 
 // 	},
--- ambika_accounts/ambika_accounts/doctype/auto_outstanding/test_auto_outstanding.py
+++ ambika_accounts/ambika_accounts/doctype/auto_outstanding/test_auto_outstanding.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestAutoOutsatnding(FrappeTestCase):
+class TestAutoOutstanding(FrappeTestCase):
 	pass
