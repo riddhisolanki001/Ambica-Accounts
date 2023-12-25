d8e200e014deed42099a32360a29fad280a5c5b2
riddhi_solanki <riddhssolanki048@gmail.com>
2023-12-25 13:36:15 +0530

    new doctype

---
+++ ambika_accounts/ambika_accounts/doctype/auto_outstannding_adjustment/auto_outstannding_adjustment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Riddhi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Auto Outstannding Adjustment", {
+// 	refresh(frm) {
+
+// 	},
+// });
