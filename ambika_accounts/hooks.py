2893def2e8ead8fa61b91cb48030560af3ca5074
riddhi_solanki <riddhssolanki048@gmail.com>
2023-12-25 11:26:28 +0530

    final commit

--- ambika_accounts/hooks.py
+++ ambika_accounts/hooks.py
@@ -114,9 +114,9 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-override_doctype_class = {
-	"Purchase Invoice": "ambika_accounts.ambika_accounts.purchase_invoice.PurchaseInvoice"
-}
+# override_doctype_class = {
+# 	"Purchase Invoice": "ambika_accounts.ambika_accounts.purchase_invoice.PurchaseInvoice"
+# }
 
 # Document Events
 # ---------------
